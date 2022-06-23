7b0539d22f6647d9059c495e3108a329a6e26460
Jack Gibbs <jackgibbs66@icloud.com>
2022-06-23 16:25:37 +0100

    Organise routes

--- demoproject.js
+++ demoproject.js
@@ -1,11 +1,13 @@
 var express = require("express");
 var path = require("path");
-var routes = require("./routes")
 
 var app = express();
 
 app.set("port", process.env.PORT || 3000);
-app.use(routes)
+
+app.use("/", require("./routes/web"));
+app.use("/api", require("./routes/api"));
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.listen(app.get("port"), function(){
